6accd706009057d33c1e92f1f7554f6fac307443
Hans <mh990731@gmail.com>
2020-10-13 22:04:26 +0800

    update

---
+++ niyan/get/index.php
@@ -0,0 +1,22 @@
+﻿
+<?php
+  header("Content-type: application/x-javascript");
+  $hitokoto = @include '../update/cache_hitokoto.php';
+  $hitokotouid = @include '../update/cache_hitokotouid.php';
+
+  $c = rand(0,1);
+
+  if($c){
+    $max = $hitokotouid['num'];
+    $num = mt_rand(0,$max);
+    $hiko = $hitokotouid[$num];
+  }else{
+    $max = $hitokoto['num'];
+    $num = mt_rand(0,$max);
+    $hiko = $hitokoto[$num];
+  }
+
+  if($hiko['id']){
+    $add = '&#10ID：'.$hiko['id'];}
+  echo 'document.write(\'<span title="分类：',$hiko['catname'],$add,'&#10投稿：',$hiko['author'],' @ ',$hiko['date'],'">',$hiko['text'],'</span>\');';
+?>
---
+++ niyan/json/index.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+  $hitokoto = @include '../update/cache_hitokoto.php';
+  $hitokotouid = @include '../update/cache_hitokotouid.php';
+
+  $c = rand(0,1);
+
+  if($c){
+    $max = $hitokotouid['num'];
+    $num = mt_rand(0,$max);
+    $hiko = $hitokotouid[$num];
+  }else{
+    $max = $hitokoto['num'];
+    $num = mt_rand(0,$max);
+    $hiko = $hitokoto[$num];
+  }
+
+  if($hiko['id']){
+    $add = '&#10ID：'.$hiko['id'];}
+$arr =array('id'=>$hiko['id'],'catname'=>$hiko['catname'],'text'=>$hiko['text'],'author'=>$hiko['author'],'source'=>$hiko['source'],'date'=>$hiko['date']);
+print_r (json_encode($arr, JSON_UNESCAPED_UNICODE));
+?>
